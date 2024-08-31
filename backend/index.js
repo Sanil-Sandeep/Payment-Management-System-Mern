c04a93c33dd27e4e6a13b4d5c4d9b2725095f845
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-09-01 00:02:08 +0530

    lesson 3

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -8,6 +9,16 @@ app.get('/', (request, response) => {
    return response.status(234).send('Welcome');
 });
 
-app.listen(PORT, () => {
-    console.log(`App is listning to port: ${PORT}`);
-});
+
+
+mongoose
+   .connect(mongoDBURL)
+   .then(() => {
+      console.log('App connected to database');
+      app.listen(PORT, () => {
+        console.log(`App is listning to port: ${PORT}`);
+    });
+   })
+   .catch((error) => {
+      console.log(error);
+   });
