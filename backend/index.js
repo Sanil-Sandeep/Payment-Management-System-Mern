22ba69a8f8a8edeba6f6985ac0e9c1e2b17e473b
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-08-31 23:53:06 +0530

    lesson 2

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get('/', (request, response) => {
+   console.log(request)
+   return response.status(234).send('Welcome');
+});
+
 app.listen(PORT, () => {
     console.log(`App is listning to port: ${PORT}`);
 });
