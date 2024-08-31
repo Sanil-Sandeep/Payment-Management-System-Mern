593aa2c1623833967a70ae8e7dea8131dccd1fbf
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-09-01 01:35:03 +0530

    finished

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Routes, Route} from 'react-router-dom'
+import PaymentDashbord from './pages/PaymentDashbord';
+import CreatePayment from './pages/CreatePayments';
+import ShowPayment from './pages/ShowPayment';
+import EditPayment from './pages/EditPayment';
+import DeletePayment from './pages/DeletePayment';
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
+    <Routes>
+      <Route path='/' element={<PaymentDashbord />} />
+      <Route path='/payments/create' element={<CreatePayment />} />
+      <Route path='/payments/details/:id' element={<ShowPayment />} />
+      <Route path='/payments/edit/:id' element={<EditPayment />} />
+      <Route path='/payments/delete/:id' element={<DeletePayment />} />
+    </Routes>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import {SnackbarProvider} from 'notistack'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+  <SnackbarProvider>
     <App />
-  </StrictMode>,
-)
+  </SnackbarProvider>
+  </BrowserRouter>
+); 
